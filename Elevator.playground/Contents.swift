934d790eae30256f24679a2fd4f4707e647a3b5a
kunihei <57933493+kunihei@users.noreply.github.com>
2021-06-14 08:10:46 +0900

    Edit timer

--- Elevator.playground/Contents.swift
+++ Elevator.playground/Contents.swift
@@ -1,27 +1,47 @@
 import UIKit
 
 class VirtualElevator{
-    var building = 10
+    var building = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     var callButton = 2
-    var currentFloor = 3
+    var currentFloor = 6
+    var closeCount = 0
+    var callTimer:Timer = Timer()
+    var closeTimer:Timer = Timer()
     
+    //エレベーターを呼び出し呼び出された階に移動させるメソッド
     func elevatorCall() {
         if callButton != currentFloor {
             print("現在呼び出しています！")
+            callTimer = Timer.scheduledTimer(timeInterval: 0.9, target:self, selector:#selector(callCountDown), userInfo:nil, repeats:true)
         } else {
             print("扉が開きます！")
-            return
+            closeDoor()
         }
-        
-        for i in 1...10 {
-            currentFloor -= 1
-            if callButton == currentFloor{
-                print("扉が開きます")
-                return
-            }
+    }
+    
+    @objc func callCountDown(){
+        print(currentFloor)
+        currentFloor -= 1
+        if callButton == currentFloor {
+            print("扉が開きます！")
+            callTimer.invalidate()
+            closeDoor()
         }
+    }
+    
+    func closeDoor(){
+        closeTimer = Timer.scheduledTimer(timeInterval: 0.9, target:self, selector:#selector(closeDoorCountUp), userInfo:nil, repeats:true)
         
     }
+    
+    @objc func closeDoorCountUp(){
+        closeCount += 1
+        if closeCount == 20 {
+            print("扉が閉まります！")
+            closeTimer.invalidate()
+        }
+    }
+    
 }
 
 let elevator = VirtualElevator()
