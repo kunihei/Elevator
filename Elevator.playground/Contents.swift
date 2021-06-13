7a65a1542d31e90ddeecfba4b7f97479e4b05765
kunihei <57933493+kunihei@users.noreply.github.com>
2021-06-13 12:34:21 +0900

    call&desFloorMove

--- Elevator.playground/Contents.swift
+++ Elevator.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class VirtualElevator{
+    var building = 10
+    var callButton = 2
+    var currentFloor = 3
+    
+    func elevatorCall() {
+        if callButton != currentFloor {
+            print("現在呼び出しています！")
+        } else {
+            print("扉が開きます！")
+            return
+        }
+        
+        for i in 1...10 {
+            currentFloor -= 1
+            if callButton == currentFloor{
+                print("扉が開きます")
+                return
+            }
+        }
+        
+    }
+}
+
+let elevator = VirtualElevator()
+elevator.elevatorCall()
+
